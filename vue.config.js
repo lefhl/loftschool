63d5761b488c0bd19cef1fe42b793c27c0dd71bf
edik <gizbreht.e@yandex.ru>
2022-01-16 21:47:46 +0300

    #4

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/3/'
+    ? '/loftschool/'
     : '/',
   configureWebpack: {
     resolve: {
